d7fd6ec1f2b899be1176de9630bb56c9fe450ece
Sidhida Behera <sidhidab@gmail.com>
2025-02-01 20:54:22 +1100

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -4,6 +4,82 @@
 
 #define MAX_SIZE 25  
 
+
+int solveSudoku(int **board, int size, int boxSize);
+int isValid(int **board, int size, int boxSize, int row, int col, int num);
+void printBoard(int **board, int size, int boxSize);
+void getUserInput(int **board, int size, int boxSize);
+
+
+int main(void) {
+    int size, boxSize;
+    printf("Enter Sudoku size: ");
+    scanf("%d", &size);
+
+    boxSize = sqrt(size);
+    if (boxSize * boxSize != size || size > MAX_SIZE) {
+        printf("The sizse must be a perfect square and ≤ %d×%d.\n", MAX_SIZE, MAX_SIZE);
+        return 1;
+    }
+
+    int **board = (int **)malloc(size * sizeof(int *));
+    for (int i = 0; i < size; i++)
+        board[i] = (int *)malloc(size * sizeof(int));
+
+    getUserInput(board, size, boxSize);  
+
+    printf("\nUnsolved Sudoku:\n");
+    printBoard(board, size, boxSize);
+
+    if (solveSudoku(board, size, boxSize)) {
+        printf("\nSolved Sudoku:\n");
+        printBoard(board, size, boxSize);
+    } else {
+        printf("\nNo solution exists :(\n");
+    }
+
+    for (int i = 0; i < size; i++)
+    free(board[i]);
+    free(board);
+
+    return 0;
+}
+
+int solveSudoku(int **board, int size, int boxSize) {
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            if (board[row][col] == 0) {
+                for (int num = 1; num <= size; num++) {
+                    if (isValid(board, size, boxSize, row, col, num)) {
+                        board[row][col] = num;
+                        if (solveSudoku(board, size, boxSize)) return 1;
+                        board[row][col] = 0;
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int isValid(int **board, int size, int boxSize, int row, int col, int num) {
+    for (int i = 0; i < size; i++) {
+        if (board[row][i] == num || board[i][col] == num)
+            return 0;
+    }
+
+    int boxRow = row - row % boxSize;
+    int boxCol = col - col % boxSize;
+    for (int i = 0; i < boxSize; i++) {
+        for (int j = 0; j < boxSize; j++) {
+            if (board[boxRow + i][boxCol + j] == num)
+                return 0;
+        }
+    }
+    return 1;
+}
+
 void printBoard(int **board, int size, int boxSize) {
     printf("\n");
     for (int i = 0; i < size; i++) {
@@ -39,74 +115,4 @@ void getUserInput(int **board, int size, int boxSize) {
             }
         }
     }
-}
-
-
-int isValid(int **board, int size, int boxSize, int row, int col, int num) {
-    for (int i = 0; i < size; i++) {
-        if (board[row][i] == num || board[i][col] == num)
-            return 0;
-    }
-
-    int boxRow = row - row % boxSize;
-    int boxCol = col - col % boxSize;
-    for (int i = 0; i < boxSize; i++) {
-        for (int j = 0; j < boxSize; j++) {
-            if (board[boxRow + i][boxCol + j] == num)
-                return 0;
-        }
-    }
-    return 1;
-}
-
-int solveSudoku(int **board, int size, int boxSize) {
-    for (int row = 0; row < size; row++) {
-        for (int col = 0; col < size; col++) {
-            if (board[row][col] == 0) {
-                for (int num = 1; num <= size; num++) {
-                    if (isValid(board, size, boxSize, row, col, num)) {
-                        board[row][col] = num;
-                        if (solveSudoku(board, size, boxSize)) return 1;
-                        board[row][col] = 0;
-                    }
-                }
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-int main() {
-    int size, boxSize;
-    printf("Enter Sudoku size: ");
-    scanf("%d", &size);
-
-    boxSize = sqrt(size);
-    if (boxSize * boxSize != size || size > MAX_SIZE) {
-        printf("The sizse must be a perfect square and ≤ %d×%d.\n", MAX_SIZE, MAX_SIZE);
-        return 1;
-    }
-
-    int **board = (int **)malloc(size * sizeof(int *));
-    for (int i = 0; i < size; i++)
-        board[i] = (int *)malloc(size * sizeof(int));
-
-    getUserInput(board, size, boxSize);  
-
-    printf("\nUnsolved Sudoku:\n");
-    printBoard(board, size, boxSize);
-
-    if (solveSudoku(board, size, boxSize)) {
-        printf("\nSolved Sudoku:\n");
-        printBoard(board, size, boxSize);
-    } else {
-        printf("\nNo solution exists :(\n");
-    }
-
-    for (int i = 0; i < size; i++)
-    free(board[i]);
-    free(board);
-
-    return 0;
 }
